6386e876ab0fbf1310da1d46c2af1deb2f098476
giovanniramirez <giovanni.ramirez19@itca.edu.sv>
2020-06-18 09:45:27 -0600

    Create gracias.php

---
+++ gracias.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1 align="center "> Gracias por enviar este comentario </h1>
+<a href="index.php">Regresar</a>
+
+</body>
+</html>
